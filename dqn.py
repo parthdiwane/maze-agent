188b54db4b6c2e50cfcad86077e3b1b60fa8b108
parthdiwane <parthdiwane@gmail.com>
2025-09-05 14:45:39 -0700

    added dqn and replay memory

---
+++ dqn.py
@@ -0,0 +1,45 @@
+import math
+import random
+import matplotlib
+import matplotlib.pyplot as plt 
+from collections import namedtuple, deque
+from itertools import count 
+
+import torch 
+import torch.nn as nn
+import torch.optim as optim 
+import torch.nn.functional as F
+
+# matplot lib setup
+is_ipython = 'inline' in matplotlib.get_backend()
+if is_ipython:
+    from iPython import display
+
+Transition = namedtuple('Transition', ('state', 'action', 'next_state', 'reward'))
+
+class ReplayMemory(object):
+    def __init__(self, capacity):
+        self.memory = deque([],maxlen=capacity)
+    
+    def push(self, *args):
+        """ saves the transition"""
+        self.memory.append(Transition(*args))
+    
+    def sample(self, batch_size):
+        return random.sample(self.memory, batch_size)
+
+    def __len__(self):
+        return len(self.memory)
+
+class DQN(nn.Module):
+    def __init__(self, n_observations, n_actions):
+        super(DQN, self).__init__()
+        self.layer1 = nn.Linear(n_observations,128)
+        self.layer2 = nn.Linear(128,128)
+        self.layer3 = nn.Linear(128, n_actions)
+
+    def forward(self, x):
+        x = F.relu(self.layer1(x))
+        x = F.relu1(self.layer2(x))
+        return self.layer3(x)
+    
